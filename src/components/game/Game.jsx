ad738f395e1931610dcbe1176a147041990acaae
Luiz Henrique <luizgarciaevilalva@gmail.com>
2022-12-01 16:36:29 -0300

    criado estados no game option

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,19 +1,22 @@
+import { useState } from 'react'
+
 import styles from './Game.module.css'
 
-import GameOption from "../gameOption/GameOption"
+import GameOption from '../gameOption/GameOption'
 
 function Game () {
+  const [gameState,setGameState] = useState (Array(9).fill(0))
+ 
   return (
     <div className= {styles.game}>
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
+    {
+      gameState.map((value, pos) => 
+        <GameOption 
+          key= {`game-option-pos-${pos}`}
+          status= {value}
+        />
+      )
+    }
     </div>
   )
 }
--- src/components/gameOption/GameOption.jsx
+++ src/components/gameOption/GameOption.jsx
@@ -1,8 +1,17 @@
 import styles from './GameOption.module.css'
+import Icon from '../icon/Icon'
 
-function GameOption () {
+const GameIcon= ({iconName}) => <Icon iconName= {iconName} size= '25px' />
+
+function GameOption ({status}) {
   return (
     <div className= {styles.gameOption}>
+      {
+        status === 1 &&  <GameIcon iconName= 'circle' />
+      }
+      {
+        status === -1 &&  <GameIcon iconName= 'x' />
+      }
     </div>
   )
 }
