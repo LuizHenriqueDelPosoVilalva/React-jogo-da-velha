8db04ef34f95f2d0d4cd41f035c43802a006dac4
Luiz Henrique <luizgarciaevilalva@gmail.com>
2022-12-07 18:25:27 -0300

    adicionado useEffect

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,22 +1,50 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 
 import styles from './Game.module.css'
 
 import GameOption from '../gameOption/GameOption'
 import Icon from '../icon/Icon'
 
+const winnerTable = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
+
 function Game () {
   const [gameState,setGameState] = useState (Array(9).fill(0))
-  const [currentPlayer, setCurrentPlayer] = useState (1)
+  const [currentPlayer, setCurrentPlayer] = useState (-1)
+  const [winner, setWinner] = useState (0)
 
    const handleClick= (pos) => {
-    if (gameState[pos] === 0) {
+    if (gameState[pos] === 0 && winner === 0) {
       let newGameState= [...gameState]
       newGameState[pos]= currentPlayer
-      setCurrentPlayer (currentPlayer *-1)
       setGameState (newGameState)
     }
   }
+
+  const verifyGame= () => {
+    winnerTable.forEach((line) => {
+      const values= line.map((pos) => gameState[pos])
+      const sum= values.reduce((sum,value) => sum + value )
+     if (sum === 3 || sum === -3) {
+      setWinner(sum/ 3)
+      console.log('VENCEDOR: ', sum/3)
+      }
+    })
+  }
+   
+
+  useEffect(() => {
+    setCurrentPlayer (currentPlayer *-1)
+    verifyGame ()
+  }, [gameState])
  
   return (
     <div className= {styles.gameContent}>
